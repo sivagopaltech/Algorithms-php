f1fd9ccc320279ccc833831fd3629a299e9d99a3
sivagopaltech <sivagopaltech@gmail.com>
2014-11-01 15:20:30 +0530

    Update mathlib.php

--- mathlib.php
+++ mathlib.php
@@ -19,5 +19,42 @@ class mathlib()
   {
     return $n*$n;
   }
+  
+  /* return prime numbers upto $n */
+  function getPrimes($n)
+  {
+    $primes = array();
+    for($i=2;$i<=$n;$i++)
+    {
+        $prime = true;
+        $limit = floor(sqrt($i));
+        for($j=2;$j<=$limit;$j++)
+        {
+            if($i%$j == 0)
+            {
+             $prime = false;
+             break;
+            }
+        }
+        if($prime==true)
+           $primes[] = $i;
+    }
+    return $primes;
+  }
+  
+  /* return Fibonacci Numbers upto $n */
+  function getFibonacciNumbers($n)
+  {
+    $fib_numbers = array();
+    $a =0;
+    $b=1;
+    while($b<=$n)
+    {
+        $fib_numbers[] = $c = $a+$b;
+        $a = $b;
+        $b = $c;
+    }
+    return $fib_numbers;
+  }
 
 }
